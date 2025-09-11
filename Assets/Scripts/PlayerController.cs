99035f0b56ffd6d6b3d8abad64ef05e341cc2e61
kstrong03 <kat.strong2003@gmail.com>
2025-09-11 09:39:38 -0400

    Ball can roll!
    added script to make the ball roll yippee

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInput : MonoBehaviour
+{
+    private float movementX;
+    private float movementY;
+
+    [SerializeField]
+    private float speed;
+
+    private Rigidbody rb;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+   /* void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }*/
+
+    private void FixedUpdate()
+    {
+
+        movementX = Input.GetAxis("Horizontal");
+        movementY = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+        rb.AddForce(movement * speed);
+
+    }
+}
